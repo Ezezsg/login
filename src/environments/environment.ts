3eeec846454ecdfac28cbddef3494567d94ee6f7
Ezezsg <ezequielfperez@gmail.com>
2020-09-07 13:06:45 -0300

    login y grilla con usuario no me actualiza heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  urlpaises: "https://restcountries.eu/rest/v2/"
+  urlpaises: "https://restcountries.eu/rest/v2/",
+  production: false
 };
 
 /*
