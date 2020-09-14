9cc28edc8ebd9b121d2fd940fe7511161fe1eaed
Ezezsg <ezequielfperez@gmail.com>
2020-09-14 00:29:28 -0300

    Parte 2-INPUT y OUTPUT terminado

--- src/app/modelo/usuario.ts
+++ src/app/modelo/usuario.ts
@@ -1,5 +1,10 @@
 export class Usuario {
-	public email; 
-	public clave;
-	public perfil;
+	id: number;
+    email: string;
+    Sexo: string;
+    Nombre: string;
+    Apellido: string;
+    FechaNac: Date;
+    FotoPerfil: string;
+    perfil: string;
 } 
